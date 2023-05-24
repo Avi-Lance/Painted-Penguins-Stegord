cf8111efa1d88e47633439a1dd59f93a3760f2f0
Avi <alance@uoregon.edu>
2023-05-24 14:30:30 -0700

    Authentication works properly, refer to Discord instructions to pull down necessary resources

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "paintedpenguinsstegof2a3d386": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
