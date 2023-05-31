eba8ffd536ea32d486b884c0e85c6598d36646e9
Avi <alance@uoregon.edu>
2023-05-30 19:29:20 -0700

    Added quick script to compile python clis to executables for ease of use on installation. Might change over to pyoxidizer instead of pyinstaller

---
+++ client/compile_python_for_dist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Make sure this script is in the folder with all the python scripts
+
+#Get the path of the script
+SCRIPT_PATH="\$0"
+
+# Set the input and output directories
+INPUT_DIR_PATH="$(dirname "$SCRIPT_PATH")"
+OUTPUT_DIR="../src/main/python_clis"
+
+python_clis=("add_friend.py" "create_chat.py" "get_messages.py" "join_chat.py"  "leave_chat.py" "list_chats.py" "list_users.py"  "send_message.py" "set_bio.py")
+
+for cli in "${python_clis[@]}"; do
+
+    # Use PyInstaller to create a standalone executable of the Python file
+    pyinstaller "$INPUT_DIR_PATH/$cli" --onefile -y --distpath "$OUTPUT_DIR"
+done
