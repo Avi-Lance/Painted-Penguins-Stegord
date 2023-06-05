3131ebd954746eafe8f420d471957f0c9a2593cc
Avi <alance@uoregon.edu>
2023-06-05 00:20:02 -0700

    Changed dashboard.tsx over to UseEffect. Note: I am currently having issues passing over the listUsers data to FindFriends.

--- src/renderer/components/Dashboard.tsx
+++ src/renderer/components/Dashboard.tsx
@@ -1,4 +1,5 @@
 import './components.css';
+import { useState, useEffect } from 'react';
 import { Layout } from 'antd';
 import { Routes, Route } from 'react-router-dom';
 import { Auth } from 'aws-amplify';
@@ -7,12 +8,56 @@ import Sidebar from './Sidebar';
 import ProfileSettings from './ProfileSettings';
 import Chat from './Chat';
 import FindFriends from './FindFriends';
+import { useSelector, useDispatch } from 'react-redux';
+import { setUsers } from '../../redux/usersSlice';
 
 const { Content } = Layout;
 
-const userdata: AmplifyUser = await Auth.currentUserInfo();
+type UserBioMap = Record<string, string>;
 
+// Define the UserBioMap type
 export default function Dashboard() {
+  const userState = useSelector((state: { users: UserBioMap }) => state.users);
+  const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<AmplifyUser | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userdata = await Auth.currentUserInfo();
+        setUser(userdata);
+
+        await window.electron.ipcRenderer.invoke(
+          'configureBackend',
+          userdata.username
+        );
+
+        const receivedUsers: UserBioMap =
+          await window.electron.ipcRenderer.invoke('listUsers', [{}]);
+
+        dispatch(setUsers(receivedUsers));
+      } catch (error) {
+        // Handle error
+        console.log('Error:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
+
+  if (loading) {
+    return (
+      <div>
+        Fetching Stegographied Data. This may take a while, but will speed up
+        use.
+      </div>
+    );
+  }
+
   return (
     <Layout className="body_dashboard">
       <Sidebar />
@@ -23,10 +68,7 @@ export default function Dashboard() {
             <Route
               path="/my_profile"
               element={
-                <ProfileSettings
-                  username={userdata?.username}
-                  bio="Sample bio"
-                />
+                <ProfileSettings username={user?.username} bio="Sample bio" />
               }
             />
             <Route path="/find_friends" element={<FindFriends />} />
--- src/renderer/components/FindFriends.tsx
+++ src/renderer/components/FindFriends.tsx
@@ -1,22 +1,29 @@
-import { useState } from 'react';
 import User from './User';
+import { useSelector } from 'react-redux';
+
+// Assuming the user slice state is defined as `UserState`
+interface UserState {
+  users: UserBioMap;
+}
+
+// Define the UserBioMap type
+type UserBioMap = Record<string, string>;
 
 export default function FindFriends() {
-  const [users, setUsers] = useState({'Loading Users...' : 'temporary'});
+  const userState = useSelector((state: { users: UserState }) => state.users);
+  console.log(userState);
+  if (!userState?.users) {
+    return <div>Loading...</div>;
+  }
 
-  window.electron.ipcRenderer.sendMessage('listUsers', [null]);
-
-  window.electron.ipcRenderer.on('listUsers', async (event, arg) => {
-    setUsers(event);
-  });
-
-  console.log(users);
+  const users = userState?.users;
+  console.log(userState);
 
   return (
     <div className="friend_list">
-      {Object.keys(users).map((user) => {
-        return <User name={user} />;
-      })}
+      {Object.keys(users).map((user) => (
+        <User key={user} name={user} />
+      ))}
     </div>
   );
 }
