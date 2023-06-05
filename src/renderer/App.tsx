7ce874a6fc08e71765654948958aa7dc2506c3c6
Avi <alance@uoregon.edu>
2023-06-05 00:15:52 -0700

    Added provider for Redux Store

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,26 +1,27 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
-
+import { Provider } from 'react-redux';
 // Uncomment for backend
 
 import { Amplify, Auth } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 import { Authenticator } from '@aws-amplify/ui-react';
 import awsExports from '../aws-exports';
+import store from '../redux/store';
 
 Amplify.configure(awsExports);
 
 export default function App() {
   return (
     <Authenticator>
-      {({ user }) => (
+      <Provider store={store}>
         <main>
           <BrowserRouter>
             <Dashboard />
           </BrowserRouter>
         </main>
-      )}
+      </Provider>
     </Authenticator>
   );
 }
