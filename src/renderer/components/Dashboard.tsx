24fb745244891ce65fe742333b806f82f564d248
Isaac Perkins <iperkins@uoregon.edu>
2023-06-05 22:06:47 -0700

    Let eslin auto format

--- src/renderer/components/Dashboard.tsx
+++ src/renderer/components/Dashboard.tsx
@@ -13,50 +13,47 @@ const { Content } = Layout;
 
 type UserBioMap = Record<string, string>;
 
-
-
 // Define the UserBioMap type
 export default function Dashboard() {
-  const [users, setUsers] = useState({'name' : 'username'});
+  const [users, setUsers] = useState({ name: 'username' });
   const [user, setUser] = useState<AmplifyUser | null>(null);
-  const [bio, setBio] = useState<string>("N/A");
+  const [bio, setBio] = useState<string>('N/A');
 
-
-  function listUsers(){
-    return window.electron.ipcRenderer.invoke('listUsers', [{}]).then(data => {
-      let users = JSON.parse(JSON.stringify(data))
-      setUsers(users)
-      return users
-    });
+  function listUsers() {
+    return window.electron.ipcRenderer
+      .invoke('listUsers', [{}])
+      .then((data) => {
+        const users = JSON.parse(JSON.stringify(data));
+        setUsers(users);
+        return users;
+      });
   }
 
-  
-  function getUserBio(){
-    return window.electron.ipcRenderer.invoke('getUserBio', user?.username).then(data => {
-      setBio(data)
-      console.log(data)
-  });
+  function getUserBio() {
+    return window.electron.ipcRenderer
+      .invoke('getUserBio', user?.username)
+      .then((data) => {
+        setBio(data);
+        console.log(data);
+      });
   }
-
 
   useEffect(() => {
-      Auth.currentUserInfo().then((userdata)=>{
-          window.electron.ipcRenderer.invoke(
-            'configureBackend',
-            [user?.username]
-          ).then(()=> {
-            return listUsers();
-          }).then((users)=> {
-            console.log(users)
-            console.log(userdata)
-            console.log(users[userdata?.username])
-            setBio(users[userdata?.username]);
-            setUser(JSON.parse(JSON.stringify(userdata)))
-          });
-  })}, []);
-
-  
-    
+    Auth.currentUserInfo().then((userdata) => {
+      window.electron.ipcRenderer
+        .invoke('configureBackend', [user?.username])
+        .then(() => {
+          return listUsers();
+        })
+        .then((users) => {
+          console.log(users);
+          console.log(userdata);
+          console.log(users[userdata?.username]);
+          setBio(users[userdata?.username]);
+          setUser(JSON.parse(JSON.stringify(userdata)));
+        });
+    });
+  }, []);
 
   return (
     <Layout className="body_dashboard">
@@ -67,11 +64,12 @@ export default function Dashboard() {
             <Route path="/chat" element={<Chat />} />
             <Route
               path="/my_profile"
-              element={
-                <ProfileSettings username={user?.username} bio={bio} />
-              }
+              element={<ProfileSettings username={user?.username} bio={bio} />}
             />
-            <Route path="/find_friends" element={<FindFriends users={users}/>} />
+            <Route
+              path="/find_friends"
+              element={<FindFriends users={users} />}
+            />
           </Routes>
         </div>
       </Content>
